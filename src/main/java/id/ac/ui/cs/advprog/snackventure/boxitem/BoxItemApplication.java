7acbc344268962a96ec92498dcee3b2befc2c171
Fahmi Ramadhan <51454564+fahmi-ramadhan@users.noreply.github.com>
2024-04-16 08:43:52 +0700

    initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/snackventure/boxitem/BoxItemApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.snackventure.boxitem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoxItemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoxItemApplication.class, args);
+    }
+
+}
