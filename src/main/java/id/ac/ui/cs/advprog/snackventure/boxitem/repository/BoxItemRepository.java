844e704e53a49c6e1aca3725892fcf4fb7f68c19
Sylphiann <Hydrolilium@gmail.com>
2024-05-18 13:38:01 +0700

    Add an additional query method in the JpaRepository for BoxItem interface

--- src/main/java/id/ac/ui/cs/advprog/snackventure/boxitem/repository/BoxItemRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/snackventure/boxitem/repository/BoxItemRepository.java
@@ -1,10 +1,17 @@
 package id.ac.ui.cs.advprog.snackventure.boxitem.repository;
 
+import id.ac.ui.cs.advprog.snackventure.boxitem.model.Box;
 import id.ac.ui.cs.advprog.snackventure.boxitem.model.BoxItem;
+import id.ac.ui.cs.advprog.snackventure.boxitem.model.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface BoxItemRepository extends JpaRepository<BoxItem, UUID>{
+    @Query("select b from BoxItem b where b.box.id = ?1")
+    List<Item> findAllItemsInBox(UUID box_id);
 }
